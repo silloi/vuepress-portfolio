3a1741652b4976a90fc46bb503f3550803d44460
silloi <silloi1993@gmail.com>
2021-06-27 20:59:55 +0900

    feat: add locale condig and ja pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,29 @@ module.exports = {
   lang: 'en-US',
   title: 'Portfolio',
   description: 'Portfolio of silloi',
-
+  locales: {
+    // The key is the path for the locale to be nested under.
+    // As a special case, the default locale can use '/' as its path.
+    '/': {
+      lang: 'en-US',
+      title: 'Portfolio',
+      description: 'Portfolio of Silloi',
+    },
+    '/ja/': {
+      lang: 'ja_JP',
+      title: 'Portfolio',
+      description: 'Portfolio of Silloi',
+    },
+  },
   themeConfig: {
+    locales: {
+      '/': {
+        selectLanguageName: 'English',
+      },
+      '/ja/': {
+        selectLanguageName: '日本語',
+      },
+    },
     navbar: [
       // NavbarItem
       {
---
+++ docs/ja/README.md
@@ -0,0 +1,20 @@
+---
+home: true
+title: ホーム
+heroImage: /images/symbol.svg
+actions:
+  - text: 履歴書
+    link: /resume/
+    type: primary
+  - text: Introduction
+    link: /guide/
+    type: secondary
+features:
+  - title: 工学
+    details: Webアプリケーションエンジニアとして2年以上の業務経験
+  - title: 社会学
+    details: 行為論に焦点を当てた社会批評の執筆
+  - title: 科学
+    details: 放射化学を専攻、今は量子化学に興味
+footer: MIT Licensed | Copyright © 2021-present Sillos Silloi
+---
